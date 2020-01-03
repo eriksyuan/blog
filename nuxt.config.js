2d16c611817934822b7c85e5cf49216bb40f29d3
eriksyuan <eriksyuan94@outlook.com>
2020-01-03 15:26:13 +0800

    pre

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,6 @@ module.exports = {
   */
   plugins: [
     '@/plugins/antd-ui',
-    '@/plugins/highlight',
     '@/plugins/axios',
     '@/plugins/svg'
   ],
--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -1,20 +1,42 @@
 <template>
   <div>
-    <top-image type="detail" :title="data.detail.title" :time="date" :image="data.detail.image" :readNum="String(data.detail.readNum)" />
+    <top-image
+      type="detail"
+      :title="data.detail.title"
+      :time="date"
+      :image="data.detail.image"
+      :readNum="String(data.detail.readNum)"
+    />
     <div class="detail" v-highlight v-html="content"></div>
-    <a-divider >感谢 阅读</a-divider>
-    <next-post :last="data.last" :next="data.next"/>
+    <a-divider>感谢 阅读</a-divider>
+    <next-post :last="data.last" :next="data.next" />
   </div>
 </template>
 
 
 <script>
-import mavonEdit from "mavon-editor";
 import marked from "marked";
+import hljs from "highlight.js";
+import "highlight.js/styles/androidstudio.css"; // 代码高亮风格，选择更多风格需导入 node_modules/hightlight.js/styles/ 目录下其它css文件
 import axios from "axios";
 import { dateDeal } from "~/util";
 import nextPost from "~/components/common/next-post";
-import topImage from '~/components/common/top-image'
+import topImage from "~/components/common/top-image";
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  highlight: function(code) {
+    return hljs.highlightAuto(code).value;
+  }
+});
+
 export default {
   asyncData({ params, redirect }) {
     return axios
